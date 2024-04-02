3414e9953a7dc54ad9f9972a72530ae38685dc80
varunesh2006 <163394601+varunesh2006@users.noreply.github.com>
2024-04-02 07:49:07 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+
+  function calculate() {
+    var displayValue = document.getElementById('display').value;
+    try {
+      var result = eval(displayValue);
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
